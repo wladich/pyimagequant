ddbae5fdc5107be86d24d5738229056c27b168d1
Sergej Orlov <wladimirych@gmail.com>
2014-07-24 12:45:57 +0400

    fixed path to lib

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from glob import glob
 
-ext = Extension('imagequant', ['imagequant.pyx'] +
+ext = Extension('imagequant', ['imagequant/imagequant.pyx'] +
                               glob('pngquant/lib/*.c'), 
                 include_dirs=['pngquant/lib'],
                 extra_compile_args=['-std=c99'])
